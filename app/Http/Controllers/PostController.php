0db89b6a8a7df340f1b835bec15e7fe5d1a9d973
methanol-dev <thongkma001@gmail.com>
2022-03-30 15:31:09 +0700

    Sửa xóa bài viết phía user

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Comment;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Intervention\Image\Facades\Image;
 use App\Http\Requests\StorePostRequest;
-use App\Models\Comment;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UpdatePostRequest;
 
 class PostController extends Controller
 {
@@ -68,5 +70,71 @@ class PostController extends Controller
         $post->save();
 
         return redirect()->route('home');
+    }
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('user.post.update', compact('post'));
+    }
+
+    public function update(UpdatePostRequest $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $slug = Str::slug($request->title, '-');
+        if (isset($request->images)) {
+            $images = $request->images;
+            $imageName = $slug . '-' . uniqid() . Carbon::now()->timestamp . '.'
+                . $images->getClientOriginalExtension();
+
+            if (!Storage::disk('public')->exists('post')) {
+                Storage::disk('public')->makeDirectory('post');
+            }
+
+            if (Storage::disk('public')->exists('post/' . $post->images)) {
+                Storage::disk('public')->delete('post/' . $post->images);
+            }
+
+            $postImage = Image::make($images->getRealPath())->resize(752, 353, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            })->stream();
+
+            Storage::disk('public')->put('post/' . $imageName, $postImage);
+        } else {
+            $imageName = $post->images;
+        }
+        $post->user_id = Auth::id();
+        $post->title = $request->title;
+        $post->slug = $slug;
+        $post->images = $imageName;
+        $post->content = $request->content;
+        $post->save();
+
+        return redirect()->route('post.index');
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+
+        try {
+            DB::beginTransaction();
+
+            if (Storage::disk('public')->exists('post/' . $post->images)) {
+                Storage::disk('public')->delete('post/' . $post->images);
+            }
+            $post->comments()->delete();
+            $post->delete();
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            throw $th;
+        }
+
+        return redirect()->route('post.index');
     }
 }
