e8d793e736b9d95a4e57f8b844970c4d12c65c24
methanol-dev <thongkma001@gmail.com>
2022-04-22 11:29:44 +0700

    fix button update status post

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,6 @@ Route::prefix('/admin')->name('admin.')->middleware(['auth', 'admin'])->group(fu
         Route::put('/update/{id}', 'AdminPostController@update')->name('update');
         Route::delete('/destroy/{id}', 'AdminPostController@destroy')->name('destroy');
         Route::get('/approval', 'AdminPostController@approval')->name('approval');
-        Route::get('/approval/edit/{id}', 'AdminPostController@eidtApproval')->name('edit.approval');
         Route::put('/approval/update/{id}', 'AdminPostController@updateApproval')->name('update.approval');
         Route::get('/statistics', 'AdminPostController@statistics')->name('statistics');
     });
